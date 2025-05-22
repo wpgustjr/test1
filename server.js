8d990099835c97bd17b4a6550e50ddc75831f26b
unknown <jacob@example.com>
2025-05-22 23:27:10 +0900

    PORT 수정 후 재배포

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require("path");
 const fs = require("fs");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // 정적 파일 서빙
 app.use(express.static(path.join(__dirname, "public")));
